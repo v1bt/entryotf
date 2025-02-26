0120d377b4ce867ae44921277473c357099c0ba5
Virtual Byte <judong1094@gmail.com>
2025-02-26 22:25:33 +0900

    Update run.js

--- run.js
+++ run.js
@@ -1,11 +1,11 @@
-const fontUrl = 'https://rawcdn.githack.com/v1bt/balatro/312fbbc3fa794c8a3e38a4efba605612882df707/balatro.woff2';
+const efont = 'balatro';
+const font_url = `https://entryfonts.vercel.app/${efont}.woff2`;
 
 (async() => {
     try {
-        const font = await new FontFace('balatro', `url(${fontUrl})`).load();
+        const font = await new FontFace('balatro', `url(${font_url})`).load();
         document.fonts.add(font);
         Entry.playground.object.entity.setFontType('balatro');
     } catch (error) {
         console.error('댓글로 해당 메시지를 적어주세요:', error);
     }
-})();
