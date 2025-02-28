43d7ba58b38b3a8687372c7e7335ca8cf5018afd
Virtual Byte <judong1094@gmail.com>
2025-02-28 14:01:30 +0900

    Update support.js

--- examples/support.js
+++ examples/support.js
@@ -3,10 +3,11 @@ const font_url = `https://entryfonts.vercel.app/fonts/${efont}.otf`;
 
 (async() => {
     try {
-        const font = await new FontFace('balatro', `url(${font_url})`).load();
+        const font = await new FontFace(efont, `url(${font_url})`).load();
         document.fonts.add(font);
-        console.log(font);
+        Entry.playground.object.entity.setFontType(font)
+        console.log(`%c EntryOTF %c ${efont} 글꼴이 설치되었어요. %c `, 'background: black; color: white; border-radius: 5px 0px 0px 5px;', 'background: #08c490; color: white; border-radius: 0px 5px 5px 0px;', '')
     } catch (error) {
-        console.error('오류:', error);
+        console.log(`%c EntryOTF %c ${error} %c `, 'background: black; color: white; border-radius: 5px 0px 0px 5px;', 'background: #f82e3a; color: white; border-radius: 0px 5px 5px 0px;', '')
     }
 })();
