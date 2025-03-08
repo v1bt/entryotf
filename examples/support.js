c2186699ccc4bb9f27441d2190c4591be49e1008
Virtual Byte <judong1094@gmail.com>
2025-03-09 00:10:02 +0900

    Update support.js

--- examples/support.js
+++ examples/support.js
@@ -1,11 +1,11 @@
 const efont = '원하는 글꼴 이름';
 const font_url = `https://entryfonts.vercel.app/fonts/${efont}.otf`;
 
-(async() => {
+(async () => {
     try {
         const font = await new FontFace(efont, `url(${font_url})`).load();
         document.fonts.add(font);
-        Entry.playground.object.entity.setFontType(font)
+        Entry.playground.object.entity.setFontType(efont);
         console.log(`%c EntryOTF %c ${efont} 글꼴이 설치되었어요. %c `, 'background: black; color: white; border-radius: 5px 0px 0px 5px;', 'background: #08c490; color: white; border-radius: 0px 5px 5px 0px;', '')
     } catch (error) {
         console.log(`%c EntryOTF %c ${error} %c `, 'background: black; color: white; border-radius: 5px 0px 0px 5px;', 'background: #f82e3a; color: white; border-radius: 0px 5px 5px 0px;', '')
